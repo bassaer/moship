b989e9aeec4639292de645dd29447b9aadf42c95
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-04-19 22:37:19 +0900

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,24 +9,46 @@ var id: Int,
 var name: String
 )
 
+data class Account(
+var user: User,
+var item: String
+)
 
-fun jsonToObject() {
-    val jsonText = "{\"id\": 0, \"name\": \"hoge\"}"
+
+fun jsonToObject1() {
+    val jsonText = "{\"id\": 0, \"name\": \"hoge\", \"age\": 99}"
     val adapter = Moshi.Builder().build().adapter(User::class.java)
     val user = adapter.fromJson(jsonText)
     println("id: ${user?.id}, name: ${user?.name}")
 }
 
 
-fun objectToJson() {
+fun objectToJson1() {
     val user = User(999, "foo")
     val adapter = Moshi.Builder().build().adapter(User::class.java)
     val jsonText = adapter.toJson(user)
     println(jsonText)
 }
 
+fun jsonToObject2() {
+    val jsonText = "{\"user\": {\"id\": 0, \"name\": \"hoge\"}, \"item\": \"piyo\"}"
+    val adapter = Moshi.Builder().build().adapter(Account::class.java)
+    val account = adapter.fromJson(jsonText)
+    println("$account")
+}
+
+
+fun objectToJson2() {
+    val account = Account(User(999, "foo"), "bar")
+    val adapter = Moshi.Builder().build().adapter(Account::class.java)
+    val jsonText = adapter.toJson(account)
+    println(jsonText)
+}
+
 
 fun main(args: Array<String>) {
-    jsonToObject()
-    objectToJson()
+    jsonToObject1()
+    objectToJson1()
+    jsonToObject2()
+    objectToJson2()
 }
